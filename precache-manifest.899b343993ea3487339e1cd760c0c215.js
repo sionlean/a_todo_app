03ef87c9829934f25240e4e5aa81aa1b136780fd
Sionlean <sionlean@gmail.com>
2019-05-12 23:49:58 +0800

    Updates

---
+++ precache-manifest.899b343993ea3487339e1cd760c0c215.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323ccc001cf0d0f43620e312d732f995",
+    "url": "/a_todo_app/index.html"
+  },
+  {
+    "revision": "322a1747c443f637c98b",
+    "url": "/a_todo_app/static/css/2.be2b9d76.chunk.css"
+  },
+  {
+    "revision": "735c8c318223ceba726e",
+    "url": "/a_todo_app/static/css/main.b807ef7d.chunk.css"
+  },
+  {
+    "revision": "322a1747c443f637c98b",
+    "url": "/a_todo_app/static/js/2.93401871.chunk.js"
+  },
+  {
+    "revision": "735c8c318223ceba726e",
+    "url": "/a_todo_app/static/js/main.472ba8cb.chunk.js"
+  },
+  {
+    "revision": "8c580dc631aeadcf62cc",
+    "url": "/a_todo_app/static/js/runtime~main.eb38726f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/a_todo_app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/a_todo_app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/a_todo_app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/a_todo_app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/a_todo_app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/a_todo_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
