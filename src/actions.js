e4356392a9be97b1eda28f120a4d3a116b529985
Lean Jun Hui <sionlean@gmail.com>
2019-06-24 14:06:58 +0800

    updated

---
+++ src/actions.js
@@ -0,0 +1,112 @@
+export const ACTION_FILTER_UPDATE = "FILTER_CHANGE";
+export const ACTION_ITEM_ADD = "ITEM_ADD";
+export const ACTION_ITEM_RESET = "ITEM_RESET";
+export const ACTION_ITEM_DELETE = "ITEM_DELETE";
+export const ACTION_ITEM_TOGGLE = "ITEM_TOGGLE";
+export const ACTION_VIEW_TOGGLE = "VIEW_TOGGLE";
+export const ACTION_THEME_TOGGLE = "THEME_TOGGLE";
+export const ACTION_ITEM_EDIT = "ITEM_EDIT";
+export const ACTION_COMPLETED_DELETE = "COMPLETED_DELETE";
+export const ACTION_SET_TIMER = "SET_TIMER";
+export const ACTION_SET_TIMER_HOUR = "SET_TIMER_HOUR";
+export const ACTION_SET_TIMER_MIN = "SET_TIMER_MIN";
+export const ACTION_SET_TIMER_TEXT = "SET_TIMER_TEXT";
+export const ACTION_UPDATE_TITLE = "UPDATE_TITLE";
+export const ACTION_UPDATE_CATEGORY_INDEX = "UPDATE_CATEGORY_INDEX";
+export const ACTION_UPDATE_PRIORITY_INDEX = "UPDATE_PRIORITY_INDEX";
+
+export const filterUpdate = categoryIndex => ({
+  type: ACTION_FILTER_UPDATE,
+  value: categoryIndex
+});
+
+export const addItem = (title, categoryIndex, priorityIndex) => ({
+  type: ACTION_ITEM_ADD,
+  title,
+  categoryIndex,
+  priorityIndex
+});
+
+export const deleteItem = id => ({
+  type: ACTION_ITEM_DELETE,
+  id
+});
+
+export const resetItem = () => ({
+  type: ACTION_ITEM_RESET
+});
+
+export const toggleItem = id => ({
+  type: ACTION_ITEM_TOGGLE,
+  id
+});
+
+export const updateView = () => ({
+  type: ACTION_VIEW_TOGGLE
+});
+
+export const updateTheme = () => ({
+  type: ACTION_THEME_TOGGLE
+});
+
+export const editItem = (id, newTitle) => ({
+  type: ACTION_ITEM_EDIT,
+  id,
+  newTitle
+});
+
+export const deleteCompleted = () => ({
+  type: ACTION_COMPLETED_DELETE
+});
+
+export const setTimerHour = hour => ({
+  type: ACTION_SET_TIMER_HOUR,
+  hour
+});
+export const setTimerMin = min => ({
+  type: ACTION_SET_TIMER_MIN,
+  min
+});
+export const setTimerText = text => ({
+  type: ACTION_SET_TIMER_TEXT,
+  text
+});
+
+export const setTimer = () => ({
+  type: ACTION_SET_TIMER
+});
+
+export const updateCategoryIndex = categoryIndex => ({
+  type: ACTION_UPDATE_CATEGORY_INDEX,
+  categoryIndex
+});
+
+export const updatePriorityIndex = priorityIndex => ({
+  type: ACTION_UPDATE_PRIORITY_INDEX,
+  priorityIndex
+});
+
+export const updateTitle = title => ({
+  type: ACTION_UPDATE_TITLE,
+  title
+});
+
+export const actions = {
+  // This is used in conjunction with bindActionCreator
+  filterUpdate,
+  addItem,
+  deleteItem,
+  resetItem,
+  toggleItem,
+  updateView,
+  updateTheme,
+  editItem,
+  deleteCompleted,
+  setTimerHour,
+  setTimerMin,
+  setTimerText,
+  setTimer,
+  updateCategoryIndex,
+  updatePriorityIndex,
+  updateTitle
+};
---
+++ src/reducers.js
@@ -0,0 +1,184 @@
+import { combineReducers } from "redux";
+import { CreateDefaultTime, CreateListItem } from "./CreateListItem";
+import {
+  CATEGORY_ALL,
+  VIEW,
+  THEME,
+  CATEGORY_INDEX,
+  PRIORITY_INDEX
+} from "./Constants";
+import {
+  ACTION_FILTER_UPDATE,
+  ACTION_ITEM_ADD,
+  ACTION_ITEM_DELETE,
+  ACTION_ITEM_RESET,
+  ACTION_ITEM_TOGGLE,
+  ACTION_VIEW_TOGGLE,
+  ACTION_THEME_TOGGLE,
+  ACTION_ITEM_EDIT,
+  ACTION_COMPLETED_DELETE,
+  ACTION_SET_TIMER_HOUR,
+  ACTION_SET_TIMER_MIN,
+  ACTION_SET_TIMER_TEXT,
+  ACTION_SET_TIMER,
+  ACTION_UPDATE_TITLE,
+  ACTION_UPDATE_CATEGORY_INDEX,
+  ACTION_UPDATE_PRIORITY_INDEX
+} from "./actions";
+
+const DEFAULT_ITEM = new CreateListItem(
+  "hi",
+  CATEGORY_INDEX.FAMILY,
+  PRIORITY_INDEX.LOW
+);
+
+const items = (state = [DEFAULT_ITEM], action) => {
+  switch (action.type) {
+    case ACTION_ITEM_ADD:
+      const newItem = new CreateListItem(
+        action.title,
+        action.categoryIndex,
+        action.priorityIndex
+      );
+      const newItemList = [...state, newItem];
+      return newItemList;
+
+    case ACTION_ITEM_DELETE:
+      const updatedList = state.filter(item => item.id !== action.id);
+      return updatedList;
+    case ACTION_ITEM_RESET:
+      return [];
+    case ACTION_ITEM_TOGGLE:
+      state = state.map(item => {
+        if (item.id === action.id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      });
+      return state;
+    case ACTION_ITEM_EDIT:
+      const newItems = [...state];
+      for (let i = 0; i < state.length; i++) {
+        if (newItems[i].id === action.id) {
+          newItems[i].title = action.newTitle;
+        }
+      }
+      return newItems;
+    case ACTION_COMPLETED_DELETE:
+      return state.filter(item => item.completed !== true);
+    default:
+      return state;
+  }
+};
+
+const filterCategory = (state = CATEGORY_ALL, action) => {
+  switch (action.type) {
+    case ACTION_FILTER_UPDATE:
+      return action.value;
+    default:
+      return state;
+  }
+};
+
+const toggleView = (state = VIEW.LIST, action) => {
+  switch (action.type) {
+    case ACTION_VIEW_TOGGLE:
+      let newView = state === VIEW.LIST ? VIEW.BLOCK : VIEW.LIST;
+      return newView;
+    default:
+      return state;
+  }
+};
+
+const toggleTheme = (state = THEME.LIGHT, action) => {
+  switch (action.type) {
+    case ACTION_THEME_TOGGLE:
+      let newTheme = state === THEME.LIGHT ? THEME.DARK : THEME.LIGHT;
+      updateDOM(newTheme);
+      return newTheme;
+    default:
+      return state;
+  }
+};
+// Can do this in the reducers page?
+const updateDOM = newTheme => {
+  let body = document.getElementsByTagName("body")[0];
+  if (newTheme === THEME.DARK) {
+    body.classList.add(THEME.DARK);
+  } else {
+    body.classList.remove(THEME.DARK);
+  }
+};
+
+const DEFAULT_TIME = new CreateDefaultTime("", "", "");
+
+const setTimer = (state = DEFAULT_TIME, action) => {
+  switch (action.type) {
+    case ACTION_SET_TIMER_HOUR:
+      return {
+        ...state,
+        hour: action.hour
+      };
+    case ACTION_SET_TIMER_MIN:
+      return {
+        ...state,
+        min: action.min
+      };
+    case ACTION_SET_TIMER_TEXT:
+      return {
+        ...state,
+        text: action.text
+      };
+    case ACTION_SET_TIMER:
+      const time = state.min * 3000 + state.hour * 3600000;
+      setTimeout(() => {
+        alert(state.text);
+      }, time);
+      return DEFAULT_TIME;
+    //If you use "state", it will not rerender as it is pointing to the same state (Even if inner value has change)
+    //By using DEFAULT_TIME, state would have changed to something else when you typed in, hence causing it to be able to rerender when DEFAULT_STATE is used
+    default:
+      return state;
+  }
+};
+
+const updateTitle = (state = "", action) => {
+  switch (action.type) {
+    case ACTION_UPDATE_TITLE:
+      return action.title;
+    default:
+      return state;
+  }
+};
+
+const updateCategoryIndex = (state = "3", action) => {
+  switch (action.type) {
+    case ACTION_UPDATE_CATEGORY_INDEX:
+      return action.categoryIndex;
+    default:
+      return state;
+  }
+};
+
+const updatePriorityIndex = (state = "0", action) => {
+  switch (action.type) {
+    case ACTION_UPDATE_PRIORITY_INDEX:
+      return action.priorityIndex;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  items,
+  filterCategory,
+  toggleView,
+  toggleTheme,
+  setTimer,
+  updateTitle,
+  updateCategoryIndex,
+  updatePriorityIndex
+}); //This will export an object to the store via index.js
+
+// const store = createStore(reducers);
+// Essentially, Store = obeject created by reducers (STATE);
